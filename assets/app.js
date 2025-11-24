82340856a158ce4457180393d1d4f8674ab9bb65
Jhilver89 <jhilverqb@vancouver.edu.pe>
2025-11-24 15:51:41 -0500

    Add files via upload
    2

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 function enviarAyuda() {
-    const numero = "1234567890"; // <-- PON AQUÍ EL NÚMERO DE EMERGENCIA
+    const numero = "+51963973004"; // <-- PON AQUÍ EL NÚMERO DE EMERGENCIA
     const mensaje = encodeURIComponent("Necesito ayuda. Me siento en peligro en un autobús.");
     const url = `https://wa.me/${numero}?text=${mensaje}`;
 
